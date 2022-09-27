1abee7eee714a05fcae2bebb5f1bae583a50ca82
Anna Khrushkova <anna.khrushkova.27@gmail.com>
2022-09-27 19:53:06 +0300

    first commit

---
+++ test_tictactoe.py
@@ -0,0 +1,62 @@
+"""
+unittest for game TicTacToe
+"""
+import unittest
+from tictactoe import TicTacToe
+
+class TestTictactoe(unittest.TestCase):
+    "unittest class"
+
+    def test_validate_input(self):
+        "these tests check whether it was a valid input or not"
+        game = TicTacToe()
+        game.x_coord = "1.25"
+        game.y_coord = "0.567923"
+        self.assertFalse(game.validate_input())
+        game.x_coord = "a"
+        game.y_coord = "b"
+        self.assertFalse(game.validate_input())
+        game.x_coord = "2"
+        game.y_coord = "100"
+        self.assertFalse(game.validate_input())
+        game.board[0][0] = "X"
+        game.x_coord = "1"
+        game.y_coord = "1"
+        self.assertFalse(game.validate_input())
+
+        game.board[1][1] = " "
+        game.x_coord = "2"
+        game.y_coord = "2"
+        self.assertTrue(game.validate_input())
+        game.x_coord = "2"
+        game.y_coord = "2"
+        self.assertTrue(game.validate_input())
+
+    def test_check_winner(self):
+        "these tests check if someone already wins or not"
+        game = TicTacToe()
+        game.x_coord = "2"
+        game.y_coord = "2"
+        game.board[1][0] = game.board[1][1] = game.board[1][2] = "X"
+        self.assertEqual(game.check_winner(), "X wins")
+        game.x_coord = "3"
+        game.y_coord = "3"
+        game.board[0][2] = game.board[1][2] = game.board[2][2] = "O"
+        self.assertEqual(game.check_winner(), "O wins")
+        game.board[0][2] = game.board[1][1] = game.board[2][0] = "X"
+        self.assertEqual(game.check_winner(), "X wins")
+        game.board[0][0] = game.board[0][2] = "X"
+        game.board[1][1] = game.board[1][2] = game.board[2][1] = "X"
+        game.board[0][1] = game.board[1][0] = game.board[2][0] = game.board[2][2] = "O"
+        game.check_moves = 9
+        self.assertEqual(game.check_winner(), "Draw")
+        
+        game.x_coord = "3"
+        game.y_coord = "3"
+        game.board[0][2] = game.board[1][2] = game.board[2][2] = "X"
+        self.assertNotEqual(game.check_winner(), "O wins")
+        game.board[0][0] = game.board[1][1] = game.board[2][2] = "X"
+        self.assertNotEqual(game.check_winner(), "O wins")
+        
+if __name__ == "__main__":
+    unittest.main()
---
+++ tictactoe.py
@@ -0,0 +1,97 @@
+"""
+Python TicTacToe game
+"""
+class TicTacToe:
+    "this class describes a single TicTacToe game"
+
+    def __init__(self):
+        self.board = [[" " for _ in range(3)] for i in range(3)]
+        self.x_coord = 0
+        self.y_coord = 0
+        self.check_moves = 0
+
+    def show_board(self):
+        "this function displays a grid"
+        print("---------")
+        print("|", self.board[0][0], self.board[0][1], self.board[0][2], "|")
+        print("|", self.board[1][0], self.board[1][1], self.board[1][2], "|")
+        print("|", self.board[2][0], self.board[2][1], self.board[2][2], "|")
+        print("---------")
+
+    def start_game(self):
+        "this function takes the coordinates of the cell"
+        self.x_coord, self.y_coord = input("Enter the coordinates:").split()
+
+    def validate_input(self):
+        "this function checks whether it was a valid input or not"
+        if (self.x_coord.count(".") == 1 and self.x_coord.replace(".", "").isdigit()):
+            print("You should enter integers!")
+            return False
+        if (not self.x_coord.isdigit() or not self.y_coord.isdigit()):
+            print("You should enter numbers!")
+            return False
+        int_x = int(self.x_coord)
+        int_y = int(self.y_coord)
+        if (int_x < 1 or int_x > 3) or (int_y < 1 or int_y > 3):
+            print("Coordinates should be from 1 to 3!")
+            return False
+        if self.board[int_x - 1][int_y - 1] != " ":
+            print("This cell is occupied! Choose another one!")
+            return False
+        return True
+
+    def check_winner(self):
+        "this function checks if someone already wins or not"
+        int_x = int(self.x_coord) - 1
+        int_y = int(self.y_coord) - 1
+        x_wins = 0
+        o_wins = 0
+        if self.board[int_x][0] == self.board[int_x][1] == self.board[int_x][2] == "X":
+            x_wins += 1
+        if self.board[int_x][0] == self.board[int_x][1] == self.board[int_x][2] == "O":
+            o_wins += 1
+        if self.board[0][int_y] == self.board[1][int_y] == self.board[2][int_y] == "X":
+            x_wins += 1
+        if self.board[0][int_y] == self.board[1][int_y] == self.board[2][int_y] == "O":
+            o_wins += 1
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] == "X":
+            x_wins += 1
+        if self.board[0][2] == self.board[1][1] == self.board[2][0] == "X":
+            x_wins += 1
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] == "O":
+            o_wins += 1
+        if self.board[0][2] == self.board[1][1] == self.board[2][0] == "O":
+            o_wins += 1
+        if self.check_moves == 9:
+            return "Draw"
+        if x_wins == 1:
+            return "X wins"
+        if o_wins == 1:
+            return "O wins"
+        return False
+
+    def fill_process(self):
+        "this function determines who will make a move"
+        if self.check_moves % 2 == 1:
+            self.board[int(self.x_coord) - 1][int(self.y_coord) - 1] = "X"
+        else:
+            self.board[int(self.x_coord) - 1][int(self.y_coord) - 1] = "O"
+
+    def game_process(self):
+        "this function show the process of the game"
+        while True:
+            self.start_game()
+            while self.validate_input() is False:
+                self.start_game()
+            self.check_moves += 1
+            self.fill_process()
+            self.show_board()
+            if self.check_winner() is not False:
+                break
+            if self.check_winner() is False:
+                continue
+        print(self.check_winner())
+
+if __name__ == "__main__":
+    game = TicTacToe()
+    game.game_process()
