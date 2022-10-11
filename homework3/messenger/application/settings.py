c6d90740bbcdecd2f982219375d7a1247ba7e5fd
AnnieKhrush <anna.khrushkova.27@gmail.com>
2022-10-11 13:49:33 +0300

    modified third homework

--- homework3/messenger/application/settings.py
+++ homework3/messenger/application/settings.py
@@ -19,11 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gh4ko&d-kuut#o8qt879lz53ley_h#ur^4&29s$3xp_2&g)zl@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
