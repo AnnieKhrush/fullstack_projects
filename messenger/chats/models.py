5d8489f03246fd8f197a9ae22dffbae92580fc3a
AnnieKhrush <anna.khrushkova.27@gmail.com>
2022-10-18 21:40:04 +0300

    modified hw3 and hw4

--- messenger/chats/models.py
+++ messenger/chats/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 class Message(models.Model):
     message = models.TextField(max_length=256)
-    message_created_at = models.DateTimeField()
+    message_created_at = models.DateTimeField(auto_now=True)
 
 class Chat(models.Model):
     chat_title = models.CharField(max_length=32)
     chat_description = models.TextField(null=True)
-    chat_created_at = models.DateTimeField()
+    chat_created_at = models.DateTimeField(auto_now=True)
     chat_message = models.ForeignKey(Message, null=True, on_delete=models.SET_NULL)
--- messenger/users/models.py
+++ messenger/users/models.py
@@ -3,9 +3,9 @@ from chats.models import Chat
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(unique = True, max_length = 32)
-    first_name = models.CharField(max_length = 32)
-    last_name = models.CharField(max_length = 32)
+    username = models.CharField(unique=True, max_length=32)
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
     date_of_birth = models.DateField(auto_now=False)
-    user_info = models.TextField(null = True)
+    user_info = models.TextField(null=True)
     user_chat = models.ManyToManyField(Chat)
