2d2435040d10285e3ab5f862141860f912273ec4
AnnieKhrush <anna.khrushkova.27@gmail.com>
2022-10-18 21:37:57 +0300

    modified hw3 and hw4

--- messenger/users/models.py
+++ messenger/users/models.py
@@ -6,5 +6,6 @@ class User(models.Model):
     username = models.CharField(unique = True, max_length = 32)
     first_name = models.CharField(max_length = 32)
     last_name = models.CharField(max_length = 32)
+    date_of_birth = models.DateField(auto_now=False)
     user_info = models.TextField(null = True)
     user_chat = models.ManyToManyField(Chat)
