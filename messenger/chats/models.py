a34f677a43270e2b8986582aae42c163f1369812
AnnieKhrush <anna.khrushkova.27@gmail.com>
2022-10-25 19:27:53 +0300

    modified hw4

--- messenger/chats/models.py
+++ messenger/chats/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
-class Message(models.Model):
-    message = models.TextField(max_length=256)
-    message_created_at = models.DateTimeField(null=True, default=timezone.now)
-
 class Chat(models.Model):
     chat_title = models.CharField(max_length=32)
     chat_description = models.TextField(null=True)
     chat_created_at = models.DateTimeField(null=True, default=timezone.now)
-    chat_message = models.ForeignKey(Message, null=True, on_delete=models.SET_NULL)
+
+
+class Message(models.Model):
+    message = models.TextField(max_length=256)
+    message_created_at = models.DateTimeField(null=True, default=timezone.now)
+    message_in_chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
